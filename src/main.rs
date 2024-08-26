c7f82c154d7d199b9d5b2f371b88fef9e919edb7
Dueen Eduarda <dueen.eduarda@gmail.com>
2024-08-26 09:05:37 +0200

    feat: scaffold 3x3 blocks

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    layout::{Alignment, Constraint, Direction, Flex, Layout, Rect},
     style::Stylize,
-    symbols::border,
+    symbols::{self, border},
     text::Text,
     widgets::{block::Title, Block, Borders, Paragraph, Widget},
     Frame,
 };
-use std::time::Duration;
-use std::{io, time::Instant};
+use std::{
+    io,
+    time::{Duration, Instant},
+};
 
 mod tui;
 
@@ -35,15 +37,22 @@ impl App {
             if self.exit {
                 break;
             }
-            terminal.draw(|frame| self.render_frame(frame))?;
+            terminal.draw(|frame| {
+                let area = frame.area();
+
+                // self.render_frame(frame);
+                let layout = Layout::default()
+                    .direction(Direction::Vertical)
+                    .constraints([Constraint::Percentage(15), Constraint::Percentage(85)])
+                    .split(area);
+
+                render_header(frame, self, layout[0]);
+                render_table(frame, self, layout[1]);
+            })?;
 
             self.handle_events()?;
         }
         Ok(())
-    }
-
-    fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -63,11 +72,15 @@ impl App {
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
-            KeyCode::Char('i') => self.start_timer(),
+            KeyCode::Char('i') => self.start_game(),
             KeyCode::Char('p') => self.stop_timer(),
             KeyCode::Char('c') => self.continue_timer(),
             _ => {}
         }
+    }
+
+    fn start_game(&mut self) {
+        self.start_timer();
     }
 
     fn start_timer(&mut self) {
@@ -91,10 +104,11 @@ impl App {
     }
 
     fn continue_timer(&mut self) {
-        if !self.is_timer_running {
-            self.start_time = Some(Instant::now());
-            self.is_timer_running = true;
+        if self.is_timer_running {
+            return;
         }
+        self.start_time = Some(Instant::now());
+        self.is_timer_running = true;
     }
 
     fn elapsed(&self) -> Duration {
@@ -110,22 +124,129 @@ impl App {
         self.exit = true;
     }
 }
+fn render_header(f: &mut Frame, app: &mut App, area: Rect) {
+    let title = Title::from(" Welcome to Rusuku ".bold());
+
+    let header_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Fill(1); 3])
+        .split(area);
+
+    let minutes = app.elapsed().as_secs() / 60;
+    let seconds = app.elapsed().as_secs() % 60;
+    let elapsed_time = format!("{:02}:{:02}", minutes, seconds);
+    let elapsed_time = Text::from(elapsed_time.to_string().yellow().bold());
+
+    let top_middle = Block::bordered()
+        .title(title.alignment(Alignment::Center))
+        .borders(Borders::TOP | Borders::BOTTOM)
+        .border_set(border::THICK);
+
+    f.render_widget(
+        Paragraph::new(elapsed_time).centered().block(top_middle),
+        header_layout[1],
+    );
+
+    f.render_widget(
+        Block::default()
+            .borders(Borders::ALL)
+            .border_set(border::THICK),
+        header_layout[0],
+    );
+
+    f.render_widget(
+        Block::default()
+            .borders(Borders::ALL)
+            .border_set(border::THICK),
+        header_layout[2],
+    );
+}
+
+fn render_table(f: &mut Frame, _: &mut App, area: Rect) {
+    let vertical_layout = Layout::default()
+        .constraints([Constraint::Max(18); 3])
+        .direction(Direction::Horizontal)
+        .flex(Flex::Center)
+        .split(area);
+
+    for (vi, vl) in vertical_layout.iter().enumerate() {
+        let horizontal_layout = Layout::default()
+            .constraints([Constraint::Max(18); 3])
+            .direction(Direction::Vertical)
+            .split(*vl);
+
+        for (hi, hl) in horizontal_layout.iter().enumerate() {
+            let border_set = match (vi, hi) {
+                (0, 0) => symbols::border::Set {
+                    bottom_left: symbols::line::THICK_VERTICAL_RIGHT,
+                    ..symbols::border::THICK
+                },
+                (1, 0) => symbols::border::Set {
+                    top_right: symbols::line::THICK_HORIZONTAL_DOWN,
+                    top_left: symbols::line::THICK_HORIZONTAL_DOWN,
+                    bottom_left: symbols::line::THICK_CROSS,
+                    bottom_right: symbols::line::THICK_CROSS,
+                    ..symbols::border::THICK
+                },
+                (2, 0) => symbols::border::Set {
+                    bottom_right: symbols::line::THICK_VERTICAL_LEFT,
+                    ..symbols::border::THICK
+                },
+                (0, 1) => symbols::border::Set {
+                    bottom_left: symbols::line::THICK_VERTICAL_RIGHT,
+                    ..symbols::border::THICK
+                },
+                (1, 1) => symbols::border::Set {
+                    bottom_left: symbols::line::THICK_CROSS,
+                    bottom_right: symbols::line::THICK_CROSS,
+                    ..symbols::border::THICK
+                },
+                (2, 1) => symbols::border::Set {
+                    bottom_right: symbols::line::THICK_VERTICAL_LEFT,
+                    ..symbols::border::THICK
+                },
+                (0, 2) => symbols::border::THICK,
+                (1, 2) => symbols::border::Set {
+                    bottom_left: symbols::line::THICK_HORIZONTAL_UP,
+                    bottom_right: symbols::line::THICK_HORIZONTAL_UP,
+                    ..symbols::border::THICK
+                },
+                (2, 2) => symbols::border::THICK,
+                _ => symbols::border::THICK,
+            };
+
+            let borders = match (vi, hi) {
+                (0, 0) => Borders::LEFT | Borders::TOP | Borders::BOTTOM,
+                (1, 0) => Borders::ALL,
+                (2, 0) => Borders::TOP | Borders::RIGHT | Borders::BOTTOM,
+                (0, 1) => Borders::LEFT | Borders::BOTTOM,
+                (1, 1) => Borders::RIGHT | Borders::LEFT | Borders::BOTTOM,
+                (2, 1) => Borders::BOTTOM | Borders::RIGHT,
+                (0, 2) => Borders::LEFT | Borders::BOTTOM,
+                (1, 2) => Borders::LEFT | Borders::BOTTOM | Borders::RIGHT,
+                (2, 2) => Borders::BOTTOM | Borders::RIGHT,
+                _ => Borders::ALL,
+            };
+
+            Block::default()
+                .borders(borders)
+                .border_set(border_set)
+                .render(*hl, f.buffer_mut());
+        }
+    }
+}
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Title::from(" Welcome to Rusuku ".bold());
         let layout = Layout::default()
             .direction(Direction::Vertical)
-            .constraints(vec![Constraint::Percentage(15), Constraint::Percentage(85)])
+            .constraints([Constraint::Percentage(15), Constraint::Percentage(85)])
             .split(area);
 
         let header_layout = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints(vec![
-                Constraint::Percentage(33),
-                Constraint::Percentage(34),
-                Constraint::Percentage(33),
-            ])
+            .constraints([Constraint::Fill(1); 3])
             .split(layout[0]);
 
         let minutes = self.elapsed().as_secs() / 60;
